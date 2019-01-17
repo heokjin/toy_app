a02971d796fdfee62572892da06d0d0b9e0c404d
윤혁진scott.hj <scott.hj@kakaomobility.com>
2019-01-17 11:35:34 +0900

    dev

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  #one to many 관계
+  has_many :microposts
+  validates :name, presence: true
+  validates :email, presence: true    # Replace FILL_IN with the right code.
 end
