cc9f3fa5f06ba9c9fa361347d660c92d2245cbfc
윤혁진scott.hj <scott.hj@kakaomobility.com>
2019-01-18 16:25:13 +0900

    dev

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-  #one to many 관계
-  has_many :microposts
-  validates :name, presence: true
-  validates :email, presence: true    # Replace FILL_IN with the right code.
+  before_save { email.downcase! }
+
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
